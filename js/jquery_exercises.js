6de8ebd323ceb8f8a9ba708bc1abeba4c452da7e
Your Name <you@example.com>
2019-07-15 23:58:12 -0500

    jquery exercise

---
+++ js/jquery_exercises.js
@@ -0,0 +1,61 @@
+"use strict";
+
+// $(document).ready(function() {
+//     alert("I'm done!");
+// });
+
+
+
+// alert($("h1").html());
+//
+// alert($("#alert").html());
+//
+// $("li").css({"font-weight": "20px",
+//                     "color": "red",
+//                     "list-style": "none"});
+//
+// $(".codeup").css("border", "red 1px solid");
+//
+// $("button").click(function() {
+//     alert("We are now no longer the Knights Who Say Ni!");
+// });
+//
+// $("h1, p, li").css({"background-color": "lightgrey"});
+//
+// $("li").css("font-size", "20px");
+//
+// alert($("h1").text());
+
+
+
+$("h1").click(function(e) {
+    $(e.target).css("background", "dodgerblue");
+});
+
+$("h1").dblclick(function(e) {
+    $(e.target).css("background", "white");
+});
+
+$("p").dblclick(function () {
+    $(this).css("font-size", "18px");
+});
+
+$("li").hover(function() {
+    $(this).css("color", "red");
+}, function() {
+    $(this).css("color", "black");
+});
+
+
+
+$(".container").hover(function() {
+    $(this).css({"background": "salmon",
+        "color": "white"});
+}, function() {
+    $(this).css({"background": "white",
+        "color": "black"});
+});
+
+$('#textfield').keyup(function() {
+    alert('a key was released!');
+});
